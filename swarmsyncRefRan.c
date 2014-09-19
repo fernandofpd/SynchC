bb2b847d898ae20cc1db998bde036fff3ae1fe52
fernando <fernandofpd@gmail.com>
2014-09-19 19:18:00 +0100

    Separate Random Number generator
    Put cokus Mersenne Twister into its on files
    
    Conflicts:
    	swarmsyncRefRan.c
    
    Conflicts:
    	swarmsyncRefRan.c

--- swarmsyncRefRan.c
+++ swarmsyncRefRan.c
@@ -12,31 +12,13 @@
 #include "vecmatd.h"
 #include "randomd.h"
 #include "options.h"
-
+#include "cokus.h"
 #define Pi         2.*asin(1.)
-
-/* ---------- parameter for random number generator -------------------- */
-
-typedef unsigned long uint32;
-
-#define NORM 4294967295
-#define N (624)
-#define M (397)
-#define K (0x9908B0DFU)
-#define hiBit(u) ((u) & 0x80000000U)
-#define loBit(u) ((u) & 0x00000001U)
-#define loBits(u) ((u) & 0x7FFFFFFFU)
-#define mixBits(u,v) (hiBit(u)|loBits(v))
-
-static uint32 state[N+1];
-static uint32 *next;
-static int left = -1;
 
 /* ---------------------------------------------------- */
 
 void maxfind(int *p, double *Gam);
 /* random number generator */
-void seedMT(uint32 seed);
 double ranMT(void);
 /* find Q nearest neighbors of unit k */
 void Qnearest(double *px, double *py, int k, int Q, int *neigh);
@@ -148,48 +130,6 @@ void maxfind(int *p, double *Gam)
     int i;
     *p=1;
     for(i = 2; i <= S; i++) if(Gam[i] > Gam[*p]) *p = i;
-}
-
-/* random number generator cokus.c */
-void seedMT(uint32 seed)
-{
-    register uint32 x = (seed | 1U) & 0xFFFFFFFFU,*s=state;
-    register int j;
-  
-    for(left=0,*s++=x,j=N;--j;*s++ = (x*=69069U) & 0xFFFFFFFFU);
-}
-
-uint32 reloadMT(void)
-{
-    register uint32 *p0=state, *p2=state+2, *pM=state+M, s0,s1;
-    register int j;
-  
-    if(left < -1) seedMT(4357U);
-    left=N-1; next=state+1;
-    for(s0=state[0],s1=state[1],j=N-M+1;--j;s0=s1,s1=*p2++)
-    *p0++ = *pM++ ^ (mixBits(s0,s1)>>1) ^ (loBit(s1) ? K : 0U);
-    for(pM=state,j=M;--j;s0=s1,s1=*p2++)
-    *p0++ = *pM++ ^ (mixBits(s0,s1)>>1) ^ (loBit(s1) ? K : 0U);
-  
-    s1=state[0],*p0=*pM ^ (mixBits(s0,s1)>>1) ^ (loBit(s1) ? K : 0U);
-    s1 ^= (s1 >> 11);
-    s1 ^= (s1 << 7) & 0x9D2C5680U;
-    s1 ^= (s1 << 15) & 0xEFC60000U;
-    return(s1 ^ (s1 >> 18));
-}
-
-
-uint32 randomMT(void)
-{
-    uint32 y;
-  
-    if(--left < 0) return(reloadMT());
-  
-    y = *next++;
-    y ^= (y >> 11);
-    y ^= (y << 7) & 0x9D2C5680U;
-    y ^= (y << 15) & 0xEFC60000U;
-    return(y ^ (y >> 18));
 }
 
 double ranMT(void)
